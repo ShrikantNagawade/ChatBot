8f4dea68d10713f864d867b330794ae31552fbbb
ShrikantNagawade <89729774+ShrikantNagawade@users.noreply.github.com>
2022-05-21 12:54:07 +0530

    Add files via upload

---
+++ qnabotcovid777-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabotcovid777-bot-b175-Web-Deploy.pubxml -p:Password=srYvfEKd0qcp6iBxykCSKlslPrCQe1oviBAeFyPv3twWzyKppeXYdHLFQSfR
+
